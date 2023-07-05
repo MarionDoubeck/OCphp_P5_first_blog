fecde41ed6c0ef37ced676c88c08f7e22ddc96e9
Marion Doubeck <mariondoubeck@Air-de-Marion-001.lan>
2023-07-05 16:00:35 +0200

    static homepage

---
+++ app/views/contact.php
@@ -0,0 +1,20 @@
+
+<form method="post">
+    <div class="form-floating">
+        <label for="name"  class="form-label">Nom</label>
+        <input type="text" name="name" id="name" class="form-control" value="" required>
+    </div>
+    <div class="form-floating">
+        <label for="firstname"  class="form-label">Prénom</label>
+        <input type="text" name="firstname" id="firstname" class="form-control" value="" required>
+    </div>
+    <div class="form-floating">
+        <label for="email"  class="form-label">Email</label>
+        <input type="email" name="email" id="email" class="form-control" value="" required>
+    </div>
+    <div class="form-floating">
+        <label for="message" class="form-label">Votre message</label>
+        <textarea name="message" id="message" class="form-control" rows="6" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+</form>
