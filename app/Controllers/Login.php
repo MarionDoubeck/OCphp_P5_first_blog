a67870564bc7ddeb3728b95aff2f30db65822c4d
Marion Doubeck <mariondoubeck@Air-de-Marion-001.lan>
2023-07-16 13:07:27 +0200

    Login with username and password verification and CSRF token issue #10 fixes and logout draft for debugging

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -5,7 +5,7 @@ use App\Models\User;
 use App\services\Session;
 use App\services\PostGlobal;
 use App\db\DatabaseConnection;
-require_once __DIR__ .'/../helpers/csrf.php';
+require_once 'app/helpers/csrf.php';
 
 /**
  * Class login
@@ -23,55 +23,56 @@ class Login
 
     public function execute()
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Check if the CSRF token is valid
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            /* Check if the CSRF token is valid */
             if (!validateCsrfToken($_POST['csrf_token'])) {
                 die("Erreur : Jeton CSRF invalide.");
-            }
-            $username = null;
-            if (isset($_POST['username']) &&  isset($_POST['password'])
-                && !empty(trim($_POST['username'])) && !empty($_POST['password'])
-            ) {
-                $username = htmlspecialchars(trim($_POST['username']));
-               
-                $userRepository = new User();
-                $userRepository->connection = new DatabaseConnection();
-                $connectedUser = $userRepository->checkUserUsername($username);
-                if (!$connectedUser) {
-                    ?>
-                    <script language="javascript"> 
-                    alert("Mauvais pseudo");
-                    </script>
-                    <?php
-                } else {
-                    if (password_verify(
-                        trim($_POST['password']),
-                        $connectedUser->getPassword()
-                    )
-                    ) {
-                        Session::put('user_id', $connectedUser->getUser_id());
-                        Session::put('username', $connectedUser->getUsername());
-                        Session::put('role', $connectedUser->getRole());
-
+            }else{
+                $username = null;
+                if (isset($_POST['username']) &&  isset($_POST['password'])
+                    && !empty(trim($_POST['username'])) && !empty($_POST['password'])
+                ) {
+                    $username = htmlspecialchars(trim($_POST['username']));
+                
+                    $userRepository = new User();
+                    $userRepository->connection = new DatabaseConnection();
+                    $connectedUser = $userRepository->checkUserUsername($username);
+                    if (!$connectedUser) {
                         ?>
                         <script language="javascript"> 
-                        alert("Connexion réussie!");
-                        history.go(-2);
+                        alert("Mauvais pseudo");
                         </script>
                         <?php
                     } else {
-                        ?>
-                        <script language="javascript"> 
-                        alert("Mauvais mot de passe");
-                        </script>
-                        <?php
+                        if (password_verify(
+                            trim($_POST['password']),
+                            $connectedUser->getPassword()
+                        )
+                        ) {
+                            Session::put('user_id', $connectedUser->getUser_id());
+                            Session::put('username', $connectedUser->getUsername());
+                            Session::put('role', $connectedUser->getRole());
+
+                            ?>
+                            <script language="javascript"> 
+                            alert("Connexion réussie!");
+                            history.go(-2);
+                            </script>
+                            <?php
+                        } else {
+                            ?>
+                            <script language="javascript"> 
+                            alert("Mauvais mot de passe");
+                            </script>
+                            <?php
+                        }
                     }
+                } else {?>
+                    <script language="javascript"> 
+                    alert("Vous devez remplir tous les champs");
+                    </script>
+                    <?php
                 }
-            } else {?>
-                <script language="javascript"> 
-                alert("Vous devez remplir tous les champs");
-                </script>
-                <?php
             }
         }
         include 'app/views/login.php';
