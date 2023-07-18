56677ca76e1b1a4a90f20c64eef90d826b5aa4c3
Marion Doubeck <mariondoubeck@MacBook-Air-de-Marion.local>
2023-07-18 15:54:03 +0200

    few minor codacy errors

--- app/Controllers/AdminAddPost.php
+++ app/Controllers/AdminAddPost.php
@@ -12,20 +12,45 @@ use App\db\DatabaseConnection;
  * AdminAddPost class
  * To add a new post in the admin part
  */
-class AdminAddPost {
+class AdminAddPost
+{
+
+    /**
+     * Session
+     *
+     * @var Session
+     */
     private $session;
+
+    /**
+     * PostGlobal
+     *
+     * @var PostGlobal
+     */
     private $postGlobal;
+
+    /**
+     * Server
+     *
+     * @var Server
+     */
     private $server;
+
+    /**
+     * Files
+     *
+     * @var Files
+     */
     private $files;
 
 
     /**
      * Constructor that inject dependencies to avoid static access to classes like PostGlobal::get()
-     * 
-     * @param Session $session Session
+     *
+     * @param Session    $session    Session
      * @param PostGlobal $postGlobal PostGlobal
-     * @param Server $server Server
-     * @param Files $files Files
+     * @param Server     $server     Server
+     * @param Files      $files      Files
      * 
      * @return void
      */
@@ -35,7 +60,9 @@ class AdminAddPost {
         $this->postGlobal = $postGlobal;
         $this->server = $server;
         $this->files = $files;
+
     }
+
 
     /**
      * Method to add a new post
@@ -51,23 +78,23 @@ class AdminAddPost {
         $title = null;
         $chapo = null;
 
-        if ($role !='admin') {
+        if ($role !== 'admin') {
             $helper->renderView('app/views/404.php',[]);
         }
-        if ($this->server->requestMethod() === 'POST')  {
+
+        if ($this->server->requestMethod() === 'POST') {
             if ($helper->validateCsrfToken($this->postGlobal->get('csrf_token')) === FALSE) {
                 throw new \Exception("Erreur : Jeton CSRF invalide.");
             } else {
                 // We do the checks.
                 if (empty($this->postGlobal->get('content')) === FALSE && empty($this->postGlobal->get('title')) === FALSE
-                    && empty($this->postGlobal->get('chapo')) === FALSE )
-                {
+                    && empty($this->postGlobal->get('chapo')) === FALSE ) {
                     $content = strip_tags($this->postGlobal->get('content'));
                     $title = strip_tags($this->postGlobal->get('title'));
                     $chapo = strip_tags($this->postGlobal->get('chapo'));
-                    // Check if an image was uploaded
+                    // Check if an image was uploaded.
                     if (empty($this->files->file('image','tmp_name')) === FALSE ) {
-                        // Process the uploaded image
+                        // Process the uploaded image.
                         $image_data = file_get_contents($this->files->file('image','tmp_name'));
                         $image_type = $this->files->file('image','tmp_name');
                     } else {
@@ -81,7 +108,7 @@ class AdminAddPost {
                         document.location.href = '/index.php?action=adminAddPost';
                     </script>
                     <?php
-                }
+                }//end if
 
                 // We create the new article.
                 $postRepository = new Post();
@@ -97,9 +124,9 @@ class AdminAddPost {
                     </script>
                     <?php
                 }
-            }
-        }
+            }//end if
+        }//end if
         
         $helper->renderView('app/views/admin/add-post.php',[]); 
-    }
+    }//end execute()
 }
